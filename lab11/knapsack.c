aafe2f8720e7725f7995a089b21e7d671b25ba7e
Yash Virsodiya <virsodiyayash4@gmail.com>
2026-06-08 23:11:56 +0530

    Final submit

---
+++ lab11/knapsack.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+struct Obj{
+    int value;
+    int weight;
+}
+
+int comp(const void *a, const void *b) {
+    return (*(int *)a - *(int *)b);
+}
+
+void main(){
+    int[] w = {10 , 20 , 40};
+    int[] v = {60 , 80 , 100};
+    struct arr[3];
+
+    int n = sizeof(w)/sizeof(0);
+    int currentWeight = 0;
+    int totalValue = 0;
+    
+    int W = 50;
+
+    for(int i = 0 ; i < 3 ; i++){
+        
+    }
+}
